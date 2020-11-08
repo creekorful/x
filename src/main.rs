6627faca27eb7ec71556c6f3d70813bd67f6c14f
Aloïs Micard <lunamicard@gmail.com>
2020-11-08 15:12:20 +0100

    Start implementing main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-use std::env;
+use std::{env, process};
 use x::parse_operations;
 
 fn main() {
-    parse_operations(env::args());
+    let operations = match parse_operations(env::args().skip(1)) {
+        Ok(operations) => operations,
+        Err(e) => {
+            eprintln!("error while parsing operations: {}", e);
+            process::exit(1);
+        }
+    };
+
+    for operation in operations {
+        println!("{:?}", operation);
+    }
 }
